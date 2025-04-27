adedd025f599ad37d053cbebe5976fb833f940b9
meetagni <meetagnihotri@gmail.com>
2025-04-27 19:41:59 +0530

    insert

--- 014 EXERCISE-LL-Insert.py
+++ 014 EXERCISE-LL-Insert.py
@@ -81,12 +81,20 @@ class LinkedList:
             return True
         return False
     
-    ## WRITE INSERT METHOD HERE ##
-    #                            #
-    #                            #
-    #                            #
-    #                            #
-    ##############################
+    def insert(self, index, value):
+        if index<0 or index>self.length:
+            return False
+        elif index == 0:
+            return self.prepend(value)
+        elif index == self.length:
+            return self.append(value)
+        else:
+            new_node = Node(value)
+            temp = self.get(index-1)
+            new_node.next = temp.next
+            temp.next = new_node
+            self.length += 1
+            return True
   
 
 
