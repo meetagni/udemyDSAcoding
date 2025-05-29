0a732f252ff6991ede1203f275f508d3d6bc751e
meetagni <meetagnihotri@gmail.com>
2025-05-29 11:19:20 +0530

    completed DLL

--- 003 EXERCISE-DLL-Pop.py
+++ 003 EXERCISE-DLL-Pop.py
@@ -24,10 +24,26 @@ class DoublyLinkedList:
             self.head = new_node
             self.tail = new_node
         else:
-            new_node.prev = self.tail
             self.tail.next = new_node
+            new_node.prev = self.tail
             self.tail = new_node
         self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
+  
 
   
 
@@ -35,24 +51,20 @@ my_doubly_linked_list = DoublyLinkedList(1)
 my_doubly_linked_list.append(2)
 
 
-print('Head:', my_doubly_linked_list.head.value)
-print('Tail:', my_doubly_linked_list.tail.value)
-print('Length:', my_doubly_linked_list.length, '\n')
-
-print('Doubly Linked List:')
-my_doubly_linked_list.print_list()
+# (2) Items - Returns 2 Node
+print(my_doubly_linked_list.pop().value)
+# (1) Item -  Returns 1 Node
+print(my_doubly_linked_list.pop().value)
+# (0) Items - Returns None
+print(my_doubly_linked_list.pop())
 
 
 
 """
     EXPECTED OUTPUT:
     ----------------
-    Head: 1
-    Tail: 2
-    Length: 2 
-
-    Doubly Linked List:
-    1
     2
-    
+    1
+    None
+
 """
