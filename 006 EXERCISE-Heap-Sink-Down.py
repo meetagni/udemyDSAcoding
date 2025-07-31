cbd823145998d9eccb3f8f2e8c4e160df4cbdec6
meetagni <meetagnihotri@gmail.com>
2025-07-31 22:51:25 +0530

    heaps

--- 006 EXERCISE-Heap-Sink-Down.py
+++ 006 EXERCISE-Heap-Sink-Down.py
@@ -23,12 +23,23 @@ class MaxHeap:
             current = self._parent(current)
 
 
-    ## WRITE SINK_DOWN METHOD HERE ##
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
+    def _sink_down(self, index):
+        max_index = index
+        while True:
+                left_index = self._left_child(index)
+                right_index = self._right_child(index)
+                
+                if(left_index < len(self.heap) and self.heap[max_index] < self.heap[left_index]):
+                    max_index = left_index
+
+                if(right_index < len(self.heap) and self.heap[max_index] < self.heap[right_index]):
+                    max_index = right_index
+                
+                if max_index != index:
+                    self._swap(index, max_index)
+                    index = max_index
+                else:
+                    return
                        
     def remove(self):
         if len(self.heap) == 0:
